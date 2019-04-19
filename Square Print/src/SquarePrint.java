0c1e803ce870f29ef459aba563d58fc7549727b6
DESKTOP-HL9HSIK\Zain Ali <muhammadzainalijafarkhan@gmail.com>
2019-04-19 19:20:08 -0400

    Print Square

---
+++ Square Print/src/SquarePrint.java
@@ -0,0 +1,21 @@
+public class SquarePrint {
+
+	public static void main(String[]args) {
+		
+		for (int i=0; i<=10; i++) {
+			
+			for (int j=0; j<=10; j++) {
+				
+				if (j==0 || j==10 || i==0 || i==10) {
+					
+					System.out.print("*");	
+				}else {
+					
+					System.out.print(" ");
+				}
+			}
+			
+			System.out.println();
+		}
+	}
+}
